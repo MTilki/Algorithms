65c9ea4d8e9f9abf766e1f9f14a3e4d14eb22400
Mdan567 <99039167+Mdan567@users.noreply.github.com>
2022-04-04 12:17:50 +0100

    binary search test = pass

--- Binary search.py
+++ Binary search.py
@@ -1,47 +1,54 @@
+def main():
+    #create empty list
+    n=0
+    arr = []
+    target=0
 
-#create empty list
-n=0
-arr = []
-target=0
+    #input number of elements
+    n = int(input("Enter neumber of elements for the array: "))
 
-#input number of elements
-n = int(input("Enter neumber of elements for the array: "))
+    #asking for values until n
+    for i in range(0, n):
+        values = int(input())
+        #append values to the arr
+        arr.append(values)
 
-#asking for values until n
-for i in range(0, n):
-    values = int(input())
-    #append values to the arr
-    arr.append(values)
+    #ask for target value
+    target = int(input("Enter the target value"))
 
- #ask for target value
-target = int(input("Enter the target value"))
+    def binary_search(arr,target):
 
-def binary_search(arr,target):
-   
-    #define left and right of array
-    left=0
-    right= len(arr) -1
-    mid=0
-   
-    #search the array
-    while mid != target:
-        
-        mid = (left + right) // 2
-        
-        if arr[mid] <  target:
-            left = mid + 1
+        #define left and right of array
+        left=0
+        right= len(arr) -1
+        mid=0
 
-        elif arr[mid] > target:
-            right = mid - 1
-        
-        else:
-            return mid
-    return  -1
-result  = binary_search(arr, target)
+        #search the array
+        while mid != target:
+            
+            mid = (left + right) // 2
+            
+            if arr[mid] <  target:
+                left = mid + 1
+            elif arr[mid] > target:
+                right = mid - 1
+            
+            else:
+                return mid
+        return  -1
 
-if result != -1:
-    print("Target is at index", str(result))
-else:
-    print("Target not found")
+    result  = binary_search(arr, target)
 
-quit()
+    if result != -1:
+        print("Element is present at index", str(result))
+    else:
+        print("Element is not present in array")
+
+    restart = input("Search again? [Y/N]: ").upper()
+    if restart== "Y":
+        main()
+    else:
+        quit()
+    
+if __name__ == "__main__":
+    main()
